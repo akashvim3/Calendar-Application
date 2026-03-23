64c5d67751aa7948bb4c73f62e41115e59af7226
Akash <ajyak749@gmail.com>
2026-03-23 16:57:38 +0530

    bug

--- ai_assistant/settings.py
+++ ai_assistant/settings.py
@@ -17,7 +17,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key-change-in-production')
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 'yes')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+import os
+
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1,calendar-application-iiz6.onrender.com"
+).split(",")
 
 # Application definition
 INSTALLED_APPS = [
